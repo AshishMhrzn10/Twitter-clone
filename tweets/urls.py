9385089efb4dcf867d0ddadc038760e7c867800c
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-22 15:33:10 +0545

    Simple front layout

---
+++ tweets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from. import views
+
+urlpatterns = [
+    path('', views.home_view),
+    path('tweets/', views.tweet_list_view),
+    path('tweets/<int:tweet_id>/', views.tweet_detail_view),
+]
