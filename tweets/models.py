6a328c86c1e843314b3807c791122391641a9854
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-22 21:55:09 +0545

    User models and django admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import random
 
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200)
+        }
