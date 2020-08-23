2b840543a97be9f9606520fdceb79351eb391fb4
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-24 00:36:49 +0545

    Like and unlike button done

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 from django.utils.http import is_safe_url
 from .models import Tweet
 from .forms import TweetForm
-from .serializers import TweetSerializer
+from .serializers import TweetSerializer, TweetActionSerializer
 import random
 
 from rest_framework.response import Response
@@ -53,6 +53,31 @@ def tweet_delete_view(request, tweet_id, *args, **kwargs):
     obj = qs.first()
     obj.delete()
     return Response({"message": "Tweet removed"}, status=200)
+
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_action_view(request, *args, **kwargs):
+    serializer = TweetActionSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        data = serializer.validated_data
+        tweet_id = data.get("id")
+        action = data.get("action")
+
+        qs = Tweet.objects.filter(id=tweet_id)
+        if not qs.exists():
+            return Response({}, status=404)
+        obj = qs.first()
+        if action == "like":
+            obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
+        elif action == "unlike":
+            obj.likes.remove(request.user)
+        elif action == "retweet":
+            pass
+
+    return Response({}, status=200)
 
 
 @api_view(['GET'])
