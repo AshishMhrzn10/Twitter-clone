d47591c9f61d49da6be253ddb46e05f191ea2e31
Ashish Maharjan <amaharjan1032@gmail.com>
2020-08-28 11:13:27 +0545

    Followers test.py

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='ashish', password='1234')
+        self.userb = User.objects.create_user(username='ashish2', password='1234')
+
+    def test_profile_created_via_signal(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second)
+        second_user_following_whom = second.following.all()
+        qs = second_user_following_whom.filter(user=first)
+        first_user_following_no_one = first.following.all()
+        self.assertTrue(qs.exists())
+        self.assertFalse(first_user_following_no_one.exists())
